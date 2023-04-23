3e65c6aa2ae041867d6aa1af9a069bcaae7dde79
Alex Kevakian <akeva001@ucr.edu>
2023-04-22 18:07:12 -0700

    Header done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,31 @@
 import Image from "next/image";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Alex's Portfolio</title>
       </Head>
 
-      <h1>Hey GM</h1>
+      <Header />
+
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
     </div>
   );
 };
