0a7014ab2f94568a566bb75d2f6498baa78d056a
Alex Kevakian <akeva001@ucr.edu>
2023-04-22 18:07:18 -0700

    Hero done

---
+++ components/Hero.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundPattern from "./BackgroundPattern";
+
+type Props = {};
+
+const Hero = (props: Props) => {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, I'm Alex Kevakian",
+      "I-love-to-play-tennis.tsx",
+      "<ButLoveToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      {/* <BackgroundPattern /> */}
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://www.alexkevakian.com/static/media/Me2.5ddc11fc3ae865e50923.jpg"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Frontend Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="gray" />
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
