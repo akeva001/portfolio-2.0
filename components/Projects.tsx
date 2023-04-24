2eb128f3b4178af67fe7b90b09ad01c2448dbed3
Alex Kevakian <akeva001@ucr.edu>
2023-04-23 18:06:29 -0700

    mobile optimized

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,7 +18,7 @@ function Projects({}: Props) {
       </h3>
       <div
         className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory
-        z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+        z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#06c]/80"
       >
         {projects.map((project, i) => (
           <div
@@ -30,14 +30,15 @@ function Projects({}: Props) {
               transition={{ duration: 1.2 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
-              src="https://yt3.googleusercontent.com/GsP5Yvc5jOSop4SJf_75wdOYaEbO-7ZyYhnARodAGRnEMh-OQjGPGzUz2ZtzsHPtqFyHGvmbEtI=s900-c-k-c0x00ffffff-no-rj"
+              src="https://www.alexkevakian.com/static/media/TopTier.db50aac0549d071e32c4.png"
+              className="h-60 xl:h-60"
               alt=""
             />
             <div className="space-y-10 px-0 md:x-10 max-w-6xl">
-              <h4 className="text-4xl font-semibold text-center">
+              <h4 className="text-2xl md:text-4xl font-semibold text-center">
                 <span className="underline decoration-[#464646]/50">
-                  Case Study {i + 1} of {projects.length}:
-                </span>
+                  {i + 1} of {projects.length}:
+                </span>{" "}
                 UPS clone
               </h4>
               <p className="text-lg text-center md:text-left">
