1aa0ae1eca0885ccfd21ef2822738199396c994b
Alex Kevakian <akeva001@ucr.edu>
2023-04-24 23:59:17 -0700

    google analytics implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/logo3.png" />
-      </Head>{" "}
+      </Head>
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </>
   );
