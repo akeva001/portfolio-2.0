b9b0106442c641128cf9213e44559c42faaf6f3c
Alex Kevakian <akeva001@ucr.edu>
2023-05-06 21:58:35 -0700

    header animation bug fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,8 +18,8 @@ export default function Header({ handleScroll }: Props) {
   return (
     <header className="sticky top-0 p-5 flex justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
-        initial={{ x: -500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1.5 }}
         className="flex flex-row items-center"
       >
@@ -118,8 +118,8 @@ export default function Header({ handleScroll }: Props) {
       </motion.div>
 
       <motion.div
-        initial={{ x: 500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1.5 }}
         className="flex flex-row items-center text-gray-300 cursor-pointer"
       >
